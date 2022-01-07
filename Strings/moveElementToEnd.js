e8c05f2db1cd15ea94f6662727a1bb769f3fc9b3
Pooja <vrps1993@gmail.com>
2022-01-07 15:57:25 +0530

    Move element to end

---
+++ Strings/moveElementToEnd.js
@@ -0,0 +1,29 @@
+//Move Element To end
+//Time Complexity = O(N);
+//Space Complexity = O(1);
+
+function moveElementToEnd(array, toMove) {
+    // Write your code here.
+      let i = 0;
+      let j= array.length-1;
+      while(i<j){
+          while(i<j && array[j]=== toMove){
+              j--;
+          }
+          if(array[i]=== toMove){
+              swap(i,j,array);
+          }
+          i++;
+      }
+      return array;
+  }
+  
+  function swap(i,j,array){
+      const temp=array[j];
+      array[j]=array[i];
+      array[i]=temp;
+  }
+  
+  // Do not edit the line below.
+  exports.moveElementToEnd = moveElementToEnd;
+  
